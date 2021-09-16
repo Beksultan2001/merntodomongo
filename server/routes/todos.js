a5b3d226916d02e2b831585dced5a066e6fac70b
Beksultan <Beksultanomirzak.03@gmail.com>
2021-09-16 19:10:41 +0600

    first commit

---
+++ server/routes/todos.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { readTodos,createTodos } from './../controller/todo.js';
+
+
+const router = express.Router();
+router.get('/',readTodos);
+router.post('/',createTodos);
+export default router;
